47f34f856fdc46859439ae1062fb816bd5601979
Michał Jakubowski <revanmj@gmail.com>
2015-02-13 15:13:50 +0100

    Pierwszy commit
    Działa logowanie i binding danych na pierwszej stronie

---
+++ FeedbinWP/FeedbinWP.Shared/FeedbinEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using SQLite;
+
+namespace FeedbinWP
+{
+    //[Table("Entries")]
+    class FeedbinEntry
+    {
+        public FeedbinEntry(String title, String content)
+        {
+            this.title = title;
+            this.content = content;
+        }
+        //[PrimaryKey]
+        public int id { get; set; }
+        public int feed_id { get; set; }
+        public String title { get; set; }
+        public String url { get; set; }
+        public String author { get; set; }
+        public String content { get; set; }
+        public String summary { get; set; }
+        public String published { get; set; }
+        public DateTime created_at { get; set; }
+        public bool read { get; set; }
+    }
+}
---
+++ FeedbinWP/FeedbinWP.WindowsPhone/LoginPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+using System.Text;
+using Windows.UI.ViewManagement;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace FeedbinWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String username = loginField.Text;
+            String password = passwordField.Password;
+
+            StatusBarProgressIndicator progressbar = StatusBar.GetForCurrentView().ProgressIndicator;
+            progressbar.Text = "Logging in ...";
+
+            await progressbar.ShowAsync();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + password)));
+
+                HttpResponseMessage response = await client.GetAsync(new Uri("https://api.feedbin.com/v2/authentication.json"));
+                await progressbar.HideAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    var vault = new Windows.Security.Credentials.PasswordVault();
+                    vault.Add(new Windows.Security.Credentials.PasswordCredential("Feedbin", username, password));
+                    Frame rootFrame = Window.Current.Content as Frame;
+
+                    if (rootFrame != null && rootFrame.CanGoBack)
+                        rootFrame.GoBack();
+                }
+                else
+                {
+                    MessageDialog msgbox = new MessageDialog("Login unsuccesful!");
+                    await msgbox.ShowAsync();
+                }
+            }
+
+        }
+    }
+}
